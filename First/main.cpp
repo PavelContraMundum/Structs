70645bc9bb4cda6134e0e8512ff3a2232cc6cbb6
Pavel Štička <pavelsticka@Pavel--Mac-mini.local>
2024-07-21 08:43:50 +0200

    pridana trida

--- First/main.cpp
+++ First/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include<list>
 
 using namespace std;
 
@@ -17,12 +18,41 @@ struct Book {
     
 };
 
+class Author {
+private:
+    string FirstName;
+    string SecondName;
+    list<string>PublishedTitles;
+    
+public:
+    Author(string firstName, string secondName){
+        FirstName=firstName;
+        SecondName=secondName;
+    }
+    
+    
+    void printAuthor(){
+        cout << "Muj oblibeny autor je " + FirstName + " " + SecondName + "." << endl;
+        cout << "Jeho tituly jsou:" << endl;
+        for(string bookTitle: PublishedTitles){
+            cout << bookTitle << endl;
+        }
+                
+    }
+    
+    void addTile(string title){
+        PublishedTitles.push_back(title);
+    }
+    
+    
+};
+
 void printSelectedBook(Book book){
     cout << "Libi se mi " + book.title << endl;
 };
 
 int main(int argc, const char * argv[]) {
-   
+    
     Book book1;
     book1.title="Jmeno ruze";
     book1.authorFirstName="Umberto";
@@ -34,6 +64,19 @@ int main(int argc, const char * argv[]) {
     book2.authorFirstName="Jules";
     book2.authorSecondName="Verne";
     book2.year=2023;
+    
+    Author autor1("Jules", "Verne");
+    autor1.addTile("Dva roky prazdnin");
+    autor1.addTile("Dvacet tisic mil pod morem");
+    autor1.addTile("Na komete");
+    autor1.printAuthor();
+    
+    
+    Author autor2("Umberto", "Eco");
+    autor2.addTile("Jmeno ruze");
+    autor2.addTile("Tajemny plamen kralovny Loany");
+    autor2.addTile("Ostrov vcerejsiho dne");
+    autor2.printAuthor();
         
     printSelectedBook(book1);
     
